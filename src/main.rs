ab803fd2fc1cb9b03595f3776a45ea34a2816870
caasi Huang <caasi.igd@gmail.com>
2026-06-19 03:01:58 +0800

    feat: add tested greeting function

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+fn greeting() -> &'static str {
+    "Hello, world!"
+}
+
+fn main() {
+    println!("{}", greeting());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn greeting_returns_hello_world() {
+        assert_eq!(greeting(), "Hello, world!");
+    }
+}
